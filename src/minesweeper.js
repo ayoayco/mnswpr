f339042779ff96fb9923a135f00f2b2ac9b8f9f1
Ayo <ramon.aycojr@gmail.com>
2019-12-08 20:28:15 +0800

    update version

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -12,7 +12,7 @@ import { LoggerService } from '../services/logger.service.js';
 import { LeaderBoardService } from '../services/leader-board.service.js';
 import { levels } from './levels.js';
 
-const VERSION = "0.3.6a";
+const VERSION = "0.3.6";
 const MOBILE_BUSY_DELAY = 250;
 const PC_BUSY_DELAY = 500;
 const TEST_MODE = false;
