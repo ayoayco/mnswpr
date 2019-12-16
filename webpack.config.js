b3e64ed3dce9e4a0463a2ca702807dc1926d7d98
Ayo <ramon.aycojr@gmail.com>
2019-12-16 14:00:32 +0800

    code splitting and optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 module.exports = {
+    entry: {
+      main: './src/index.js',
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // all options are optional
+        filename: '[name].bundle.css',
+        chunkFilename: '[id].css',
+        ignoreOrder: false, // Enable to remove warnings about conflicting order
+      }),
+    ],
     module: {
+
       rules: [
         {
           test: /\.css$/i,
-          use:['style-loader','css-loader']
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            'css-loader',
+          ],
         },
       ],
     },
