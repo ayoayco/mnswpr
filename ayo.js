6a4c5c172484606f85d2032a35a5d7a957293bd1
Ayo <ramon.aycojr@gmail.com>
2019-12-24 17:04:47 +0800

    remove unused files

--- ayo.js
+++
@@ -1,7 +0,0 @@
-import { LeaderBoardService } from './src/modules/leader-board/leader-board';
-
-const leaderBoard = new LeaderBoardService('mw-leaders', 'mw-all');
-
-window.updateLeaders = () => {
-    leaderBoard.updateTimeStampsLeaders();
-}
